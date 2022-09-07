8a7a27514460ae241799e54a34cd7ca0c46c38ec
Allen Domingo <112440034+allen-domingo@users.noreply.github.com>
2022-09-07 22:18:15 +0000

    Turtle Exercise

--- ch02/lab/main.py
+++ ch02/lab/main.py
@@ -1,10 +1,17 @@
 
 #Part A
 weeks = 16
+print (weeks, type(weeks))
+classes_per_week = 3 
+print (classes_per_week, type(classes_per_week))
 classes = 5
+print (classes, type(classes))
 tuition = 6000
+print(tuition, type(tuition))
 cost_per_week = ((tuition / classes) / weeks)
 print("Cost per week:", cost_per_week)
+cost_per_class = (cost_per_week/classes_per_week)
+print("Okay! so the cost per class is $", cost_per_class)
 
 
 #Part B
