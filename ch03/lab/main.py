171036b34fc75be70dc9a4e0566751e6a63884b6
Allen Domingo <112440034+allen-domingo@users.noreply.github.com>
2022-09-23 19:06:50 +0000

    Lab:Turtle Racing

--- ch03/lab/main.py
+++ ch03/lab/main.py
@@ -24,40 +24,120 @@ leonardo.goto(-100,-20)
 ## 5. Your PART A code goes here
 my_turtles = [leonardo,michelangelo]
 
-#for t in my_turtles:
-  # x = random.randrange(1,101)
-  # t.forward(x)
-  # t.left(180)
-  # t.forward(x)
-  # t.left(180)
+for t in my_turtles:
+  x = random.randrange(1,101)
+  t.forward(x)
+  
+time.sleep(1)
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
+for i in range(10):
+  time.sleep(.7)
+  for t in my_turtles:
+    x = random.randrange(1,11)
+    t.forward(x)
 
-# for i in range(10):
-#   time.sleep(.7)
-#   for t in my_turtles:
-#     x = random.randrange(1,11)
-#     t.forward(x)
-
-
+time.sleep(2)
+window.clear()
 # PART B - complete part B here
 pygame.init()
 
-window = pygame.display.set_mode()
+window2 = pygame.display.set_mode()
 
 color = "purple"
 
-num_sides = 3
-side_length = 20
-offset = 20
-times = 3
-coords = (20,20)
-
-for i in range(num_sides):
-  theta = (2.0*math.pi*times/num_sides)
-  x = side_length*math.cos(theta)+offset
-  y = side_length*math.sin(theta)+offset
+def shape_points(num_sides):
+  num_sides = num_sides
+  side_length = 20
+  offset = 100
   
-  coords.append(x)
-  coords.append(y)
+  coords = []
 
-pygame.draw.polygon(window, color, coords)
-window.exitonclick()
+  for i in range(num_sides):
+    theta = (2.0*math.pi*i)/num_sides
+    x = side_length*math.cos(theta)+offset
+    y = side_length*math.sin(theta)+offset
+    coords.append([x,y])
+
+  return coords
+  
+
+pygame.draw.polygon(window2, 'blue', shape_points(4))
+pygame.display.flip()
+pygame.time.wait(2000)
+
+
+window2.fill("black")
+pygame.display.flip()
+
+def shape_points(num_sides):
+  num_sides = num_sides
+  side_length = 20
+  offset = 100
+  
+  coords = []
+
+  for i in range(num_sides):
+    theta = (2.0*math.pi*i)/num_sides
+    x = side_length*math.cos(theta)+offset
+    y = side_length*math.sin(theta)+offset
+    coords.append([x,y])
+
+  return coords
+  
+
+pygame.draw.polygon(window2, 'blue', shape_points(6))
+pygame.display.flip()
+pygame.time.wait(2000)
+pygame.display.flip()
+
+window2.fill("black")
+pygame.display.flip()
+
+
+def shape_points(num_sides):
+  num_sides = num_sides
+  side_length = 20
+  offset = 100
+  #times = 3
+  coords = []
+
+  for i in range(num_sides):
+    theta = (2.0*math.pi*i)/num_sides
+    x = side_length*math.cos(theta)+offset
+    y = side_length*math.sin(theta)+offset
+    coords.append([x,y])
+
+  return coords
+  
+
+pygame.draw.polygon(window2, 'blue', shape_points(9))
+pygame.display.flip()
+pygame.time.wait(2000)
+
+
+window2.fill("black")
+pygame.display.flip()
+
+def shape_points(num_sides):
+  num_sides = num_sides
+  side_length = 20
+  offset = 100
+  #times = 3
+  coords = []
+
+  for i in range(num_sides):
+    theta = (2.0*math.pi*i)/num_sides
+    x = side_length*math.cos(theta)+offset
+    y = side_length*math.sin(theta)+offset
+    coords.append([x,y])
+
+  return coords
+  
+
+pygame.draw.polygon(window2, 'blue', shape_points(360))
+pygame.display.flip()
+pygame.time.wait(2000)
+
+window2.fill("black")
+pygame.display.flip()
