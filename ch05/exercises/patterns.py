ae062e6f369327efd2e4f39b28670debf86f621e
Allen Domingo <112440034+allen-domingo@users.noreply.github.com>
2022-10-10 21:42:30 +0000

    Patterns exercise

--- ch05/exercises/patterns.py
+++ ch05/exercises/patterns.py
@@ -1,4 +1,19 @@
 def star_pyramid():
-  for x in star_pyramid:
-    print(x)
-star_pyramid(9)
+  rows = int(input("How many rows would you like?"))
+  s = 1
+  star_list = []
+  while s != rows+1:
+   star_list.append(s)
+   s +=1
+  for stars1 in star_list:
+    print("*"*stars1)
+
+def rstar_pyramid():
+ stars = int(input("How many rows would you like?"))
+ while stars != 0:
+   print(stars*"*")
+   stars -=1
+   
+
+star_pyramid()
+rstar_pyramid()
