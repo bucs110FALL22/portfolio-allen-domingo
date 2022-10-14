92d32a87999a9878d57733693f58395e1832422b
Allen Domingo <112440034+allen-domingo@users.noreply.github.com>
2022-10-14 19:17:32 +0000

    Ch 5 Lab

--- ch05/lab/main.py
+++ ch05/lab/main.py
@@ -1,74 +1,82 @@
 import pygame
 
 pygame.init()
-display = pygame.display.set_mode()
-max_so_far = 0
-scale = 20
-color = "blue"
-go = True
-number_start = 101
-n = number_start
-upper_limit = 20
-upper_stop = upper_limit+1
-start = 2
-stop_value = 1
+width = 400
+height=380
+display = pygame.display.set_mode(size=(width,height))
+
+
+
 new_display = pygame.transform.flip(display, False, True)
 num = []
 font_size = 50
 font_color = "green"
 max_value = 0
 
-
 msg_pos = (10,10)
-
-# while abs(n) != stop_value:
-#   if n%2 == abs(1):
-#     n = 3*n+1
-#     print(n)
-#     num.append(n)
-#   elif n%2 == 0:
-#     n = n/2
-#     print(n)
-#     num.append(n)
+max_so_far = 0
+scale = 15
+color = "blue"
+go = True
 number_start = 101
-n = number_start
+x = number_start
+upper_limit = 20
+upper_stop = upper_limit+1
+start = 2
 z = start
-count = len(num)
-print(count)
+stop_value = 1
+
+
+coords = []
 iters = {}
 iters_c = {}
-while n !=1:
- for i in range(start,upper_stop):
-   print(i)
+def sequence(n):
+ count=0
+ while True:
+   if n==1:
+     break
+  # print(i)
    if n%2 == abs(1):
-     iters[z] = i
      n = 3*n+1
-     print(n)
+    # print(n)
      num.append(n)
-     iters_c[scale*z] = -(scale*i)
-     if i > max_so_far:
-       max_so_far = i
-     z +=1
      
+     count +=1
+   
    elif n%2 == 0:
      n = n/2
-     print(n)
+     # print(n)
      num.append(n)
-   if z ==(upper_stop):
-     n = 1
-     break
+     count +=1
+ # print(count)
+ return count
+
+for l in range(start,upper_stop):
+  count = sequence(l)
+  iters[l] = count
+  iters_c[l*scale] = height-count*scale
+  coords = list(iters_c.items())
+
+  if count > max_so_far:
+    max_so_far = count
+    max_value = str(max_so_far)
+    display.blit(new_display, (0,0))
+    font_obj = pygame.font.Font(None,font_size)
+    msg = font_obj.render("the max value is " +max_value,True,font_color)
+    display.blit(msg,msg_pos)
+
+    pygame.display.flip()
+   
+  if l > start+1:
+   pygame.draw.lines(display, color, False, coords)
+   pygame.display.flip()
+
+   
+
+  pygame.time.wait(500)
+max_value = max_so_far 
+# print("num:",num)
+print("iters:",iters)
 
 
-max_value = max_so_far
-coords = list(iters_c.items())
-pygame.draw.lines(display, color, False, coords)
-pygame.display.flip()
-
-max_value = str(max_value)
-
-font_obj = pygame.font.Font(None,font_size)
-msg = font_obj.render(max_value,True,font_color)
-display.blit(msg,msg_pos)
-# display.blit(new_display, (0,0))
-pygame.display.flip()
-pygame.time.wait(2000)
+pygame.time.wait(2500)
