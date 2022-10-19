5d30d3eb7eb688a2776f1e72bce5539024cb4ded
Allen Domingo <112440034+allen-domingo@users.noreply.github.com>
2022-10-19 21:36:29 +0000

    scope exercise

---
+++ ch05/exercises/scope.py
@@ -0,0 +1,31 @@
+def multi(a=0,b=0):
+  num = 0
+  for z in range (b):
+    num +=a
+  return num
+
+def exponent(c=0,d=0):
+  num2 = c
+  for x in range (d-1):
+    num2 *= c
+  return num2
+def square(e):
+  r = multi(e,e)
+  return r
+
+def main():
+  numA = 3
+  numB = 4
+  numC = 6
+  numD = 10
+  function1 = multi(numA,numB)
+  print(function1)
+
+  function2 = exponent(numB,numC)
+ 
+  print(function2)
+
+  function3 = square(numD)
+  
+  print(function3)
+main()
---
+++ ch06/exercises/shelter.py
@@ -0,0 +1,21 @@
+class unadopted:
+  def animals():
+    dog = ("fido","bonnie")
+    cat = ("whiskers","docks")
+
+d = unadopted()
+e = unadopted()
+print(d)
+
+
+
+class adopted:
+  def animals1():
+    dog = ()
+    cat = ()
+def main():
+  s = ("fido","bonnie")
+  adopted(s)
+main()
+h = adopted()
+print(h)
