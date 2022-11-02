d3f7ba54a4ba07369d0e417d681cec4d2a020ff1
Allen Domingo <112440034+allen-domingo@users.noreply.github.com>
2022-11-02 21:51:58 +0000

    Shelter Exercise 2

--- ch07/exercises/shelter.py
+++ ch07/exercises/shelter.py
@@ -12,10 +12,15 @@ class Shelter:
     self.time_adopted = time.strftime("%d/%m/%y")
     if self.adopted == True:
       print(self.name,"arrived:",self.arrived,"adopted:",self.time_adopted)
+  def __str__(self):
+    result_str = f"{self.name}{self.type}"
+    result_str += f"\narrived:{self.arrived}"
+    result_str += f"\nadopted:{self.time_adopted}"
+    return result_str
 def main():
   Fido = Shelter("fido","Shelter")
   Fido.adoption()
-  print(Fido.id)
+  print(Fido)
 
 
 main() 
